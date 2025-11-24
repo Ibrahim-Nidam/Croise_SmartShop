bbc226bfcabbfdea3b4934e82a098cc44ad2ec80
Ibrahim Nidam <ibrahim.nidam22@gmail.com>
2025-11-24 21:48:37 +0100

    fix : added field regex to accept only letters

--- src/main/java/ma/microtech/smartshop/dto/client/ClientCreateDTO.java
+++ src/main/java/ma/microtech/smartshop/dto/client/ClientCreateDTO.java
@@ -2,8 +2,10 @@ package ma.microtech.smartshop.dto.client;
 
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Pattern;
 
 public record ClientCreateDTO(
+        @Pattern(regexp = "^[a-zA-Z\\s]+$", message = "Name must contain only letters and spaces")
         @NotBlank String name,
         @NotBlank @Email String email,
         @NotBlank String password
