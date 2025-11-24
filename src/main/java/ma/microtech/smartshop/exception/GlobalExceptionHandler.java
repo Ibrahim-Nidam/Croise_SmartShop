d6d3502b787bd0a9d57811361d6369d3085780ad
Ibrahim Nidam <ibrahim.nidam22@gmail.com>
2025-11-24 15:07:19 +0100

    SMAR-25 Handle session expiration and invalid credentials with appropriate error responses (401). #done

---
+++ src/main/java/ma/microtech/smartshop/exception/GlobalExceptionHandler.java
@@ -0,0 +1,48 @@
+package ma.microtech.smartshop.exception;
+
+import jakarta.servlet.http.HttpServletRequest;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+import java.time.LocalDateTime;
+
+@ControllerAdvice
+public class GlobalExceptionHandler {
+    record ErrorResponse (
+            LocalDateTime timestamp,
+            int status,
+            String error,
+            String message,
+            String path
+    ){}
+
+    @ExceptionHandler(Exception.class)
+    public ResponseEntity<ErrorResponse> handleGeneric(Exception ex, HttpServletRequest req){
+        ErrorResponse error = new ErrorResponse(
+                LocalDateTime.now(),
+                500,
+                "Internal Server Error",
+                ex.getMessage(),
+                req.getRequestURI()
+        );
+        return new ResponseEntity<>(error, HttpStatus.INTERNAL_SERVER_ERROR);
+    }
+
+    @ExceptionHandler(RuntimeException.class)
+    public ResponseEntity<ErrorResponse> handleRuntime(RuntimeException ex, HttpServletRequest req){
+        int status = ex.getMessage().contains("Invalid Credentials") ? 401 : 400;
+        String error = status == 401 ? "Unauthorized" : "Bad request";
+
+        ErrorResponse response = new ErrorResponse(
+                LocalDateTime.now(),
+                status,
+                error,
+                ex.getMessage(),
+                req.getRequestURI()
+        );
+        return new ResponseEntity<>(response, HttpStatus.valueOf(status));
+    }
+
+}
