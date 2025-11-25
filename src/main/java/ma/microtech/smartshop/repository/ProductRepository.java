8729568b13eb796446a8c65ecf6aa9e37ff858fd
Ibrahim Nidam <ibrahim.nidam22@gmail.com>
2025-11-25 16:52:56 +0100

    SMAR-69 Check for existing orders via repository query before deletion. #done

--- src/main/java/ma/microtech/smartshop/repository/ProductRepository.java
+++ src/main/java/ma/microtech/smartshop/repository/ProductRepository.java
@@ -3,9 +3,16 @@ package ma.microtech.smartshop.repository;
 import ma.microtech.smartshop.entity.Product;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
+import org.springframework.data.jpa.repository.Query;
 
 import java.util.List;
 
 public interface ProductRepository extends JpaRepository<Product, Long>, JpaSpecificationExecutor<Product> {
     List<Product> findByDeletedFalse();
+
+//    @Query("select COUNT(oi) > 0 from OrderItem oi where oi.product.id = :productId")
+//    boolean isUsedInAnyOrder(Long productId);
+    default boolean isUsedInAnyOrder(Long productId) {
+        return false;
+    }
 }
