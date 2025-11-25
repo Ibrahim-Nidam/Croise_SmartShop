413a614aa026d2cff1ea8d5f7769a4102f81ce1a
Ibrahim Nidam <ibrahim.nidam22@gmail.com>
2025-11-25 15:51:41 +0100

    SMAR-62 Add GET /products endpoint to ProductController, excluding deleted products. #done

--- src/main/java/ma/microtech/smartshop/controller/ProductController.java
+++ src/main/java/ma/microtech/smartshop/controller/ProductController.java
@@ -4,8 +4,13 @@ import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import ma.microtech.smartshop.dto.product.ProductCreateDTO;
 import ma.microtech.smartshop.dto.product.ProductResponseDTO;
+import ma.microtech.smartshop.dto.product.ProductSearchCriteria;
 import ma.microtech.smartshop.dto.product.ProductUpdateDTO;
+import ma.microtech.smartshop.entity.Product;
+import org.springframework.data.domain.Page;
 import ma.microtech.smartshop.service.interfaces.ProductService;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
@@ -23,5 +28,19 @@ public class ProductController {
     @PutMapping("/{id}")
     public ResponseEntity<ProductResponseDTO> updateProduct(@PathVariable Long id, @Valid @RequestBody ProductUpdateDTO dto){
         return ResponseEntity.ok(productService.updateProduct(id, dto));
+    }
+
+    @GetMapping
+    public ResponseEntity<Page<Product>> getProducts(
+            @RequestParam(required = false) String name,
+            @RequestParam(required = false) Integer stock,
+            @RequestParam(defaultValue = "0") int page,
+            @RequestParam(defaultValue = "5") int size
+    ){
+        ProductSearchCriteria criteria = new ProductSearchCriteria(name, stock);
+        Pageable pageable = PageRequest.of(page, size);
+        Page<Product> result = productService.getProducts(criteria, pageable);
+
+        return ResponseEntity.ok(result);
     }
 }
