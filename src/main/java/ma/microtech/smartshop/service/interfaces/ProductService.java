50a9055837b312dc33a955d5317594aa05ffb3d5
Ibrahim Nidam <ibrahim.nidam22@gmail.com>
2025-11-25 15:51:16 +0100

    SMAR-61 Extend ProductService with search methods using filters (e.g., name, price range). #done

--- src/main/java/ma/microtech/smartshop/service/interfaces/ProductService.java
+++ src/main/java/ma/microtech/smartshop/service/interfaces/ProductService.java
@@ -2,9 +2,15 @@ package ma.microtech.smartshop.service.interfaces;
 
 import ma.microtech.smartshop.dto.product.ProductCreateDTO;
 import ma.microtech.smartshop.dto.product.ProductResponseDTO;
+import ma.microtech.smartshop.dto.product.ProductSearchCriteria;
 import ma.microtech.smartshop.dto.product.ProductUpdateDTO;
+import ma.microtech.smartshop.entity.Product;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 
 public interface ProductService {
     ProductResponseDTO createProduct(ProductCreateDTO dto);
     ProductResponseDTO updateProduct(Long id, ProductUpdateDTO dto);
+
+    Page<Product> getProducts(ProductSearchCriteria criteria, Pageable pageable);
 }
