4985be38c33a03942cfe0d53d5efaab6bb4b7e27
Ibrahim Nidam <ibrahim.nidam22@gmail.com>
2025-11-26 15:14:32 +0100

    feat(repository) : product updated to use the new OrderItem entity

--- src/main/java/ma/microtech/smartshop/repository/ProductRepository.java
+++ src/main/java/ma/microtech/smartshop/repository/ProductRepository.java
@@ -8,11 +8,6 @@ import org.springframework.data.jpa.repository.Query;
 import java.util.List;
 
 public interface ProductRepository extends JpaRepository<Product, Long>, JpaSpecificationExecutor<Product> {
-    List<Product> findByDeletedFalse();
-
-//    @Query("select COUNT(oi) > 0 from OrderItem oi where oi.product.id = :productId")
-//    boolean isUsedInAnyOrder(Long productId);
-    default boolean isUsedInAnyOrder(Long productId) {
-        return false;
-    }
+    @Query("select COUNT(oi) > 0 from OrderItem oi where oi.product.id = :productId")
+    boolean isUsedInAnyOrder(Long productId);
 }
