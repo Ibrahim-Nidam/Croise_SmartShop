b70eb0b7bfbd6579de920c6780a2ab630339c72e
Ibrahim Nidam <ibrahim.nidam22@gmail.com>
2025-11-28 15:16:15 +0100

    SMAR-104 Add method in PaymentService to fetch payments by order id. #done

--- src/main/java/ma/microtech/smartshop/service/interfaces/PaymentService.java
+++ src/main/java/ma/microtech/smartshop/service/interfaces/PaymentService.java
@@ -1,13 +1,15 @@
 package ma.microtech.smartshop.service.interfaces;
 
+import jakarta.servlet.http.HttpServletRequest;
 import ma.microtech.smartshop.dto.payment.PaymentRequestDTO;
 import ma.microtech.smartshop.dto.payment.PaymentResponseDTO;
 import ma.microtech.smartshop.enums.PaymentStatus;
 
 import java.time.LocalDate;
+import java.util.List;
 
 public interface PaymentService {
     PaymentResponseDTO addPayment(Long orderId, PaymentRequestDTO dto);
     PaymentResponseDTO updatePaymentStatus(Long paymentId, PaymentStatus newStatus, LocalDate dateEncaissement);
-//    List<PaymentResponseDTO> getPaymentsByOrder(Long orderId, String role, Long clientId);
+    List<PaymentResponseDTO> getPaymentsByOrderId(Long orderId, HttpServletRequest request);
 }
